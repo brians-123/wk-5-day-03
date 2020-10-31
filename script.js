f12a7da004adbf1bd6e714195d7afee27f433f49
brians-123 <brian_shor@hotmail.com>
2020-10-31 13:33:51 -0700

    added modal

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-setInterval( function() {
-    var now = moment();
-    $("#timeDate").text(now.format("MMM Do YYYY, h:mm:ss a"));
-    }, 1000);
+setInterval(function () {
+  var now = moment();
+  $("#timeDate").text(now.format("MMM Do YYYY, h:mm:ss a"));
+}, 1000);
+
+$("#myModal").modal(options);
